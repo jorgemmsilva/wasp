3167affae1f4649cc3e7a79cd7191c1dc5768b99
Julius Andrikonis <julius.andrikonis@ext.iota.org>
2022-11-22 18:42:27 +0200

    GPA fixes after state change merge

--- packages/chain/statemanager/smGPA/smGPAUtils/test_utils.go
+++ packages/chain/statemanager/smGPA/smGPAUtils/test_utils.go
@@ -3,13 +3,13 @@
 
 package smGPAUtils
 
-import (
+/*import (
 	"crypto/rand"
 	"time"
 
-	"github.com/iotaledger/hive.go/kvstore/mapdb"
+	//"github.com/iotaledger/hive.go/kvstore/mapdb"
 	"github.com/iotaledger/iota.go/v3/tpkg"
-	"github.com/iotaledger/trie.go/trie"
+	//"github.com/iotaledger/trie.go/trie"
 	"github.com/iotaledger/wasp/packages/cryptolib"
 	"github.com/iotaledger/wasp/packages/isc"
 	"github.com/iotaledger/wasp/packages/isc/coreutil"
@@ -175,3 +175,4 @@ func getRandomTxID() iotago.TransactionID {
 	rand.Read(result[:])
 	return result
 }
+*/
